31ae6f33a5a4cc0d235ec82b8060c86a0d4a74cc
sahilDeb <sahildebnath091195@gmail.com>
2024-02-02 21:36:28 +0530

    fixed styles and navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,7 +39,7 @@ export const Navbar = () => {
             <a href="#experience">Experience</a>
           </li>
           <li>
-            <a href="#tech-stack">Projects</a>
+            <a href="#tech-stack">Tech Stack</a>
           </li>
           <li>
             <a href="#contact">Contact</a>
